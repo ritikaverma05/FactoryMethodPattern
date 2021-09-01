354a30fb8bf7949ff0e907ac288aad692dfb8f1a
Ritika Verma <ritika.v2000@gmail.com>
2021-09-01 10:19:30 +0530

    commit via terminal

--- FactoryMethodPattern/FactoryClass/CarSelector.swift
+++ FactoryMethodPattern/FactoryClass/CarSelector.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+
+class CarSelector{
+    
+    class func selectCar(passengers: Int)->String?{
+        return createRentalCar(passengers: passengers)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    class func selectCar(passenger: Int)-> String? {
+//
+//        var car:RentalCar?
+//
+//        switch (passenger) {
+//        case 0...2:
+//            car = Sports()
+//        case 3...4:
+//            car = Compact()
+//        case 5...7:
+//            car = SUV()
+//        case 8...20:
+//            car = Bus()
+//        default:
+//            car = nil;
+//        }
+//
+//    return car?.name
+//
+//    }
--- FactoryMethodPattern/FactoryClass/RentalCar.swift
+++ FactoryMethodPattern/FactoryClass/RentalCar.swift
@@ -5,4 +5,51 @@
 //  Created by RITIKA VERMA on 01/09/21.
 //
 
+//Global Factory Method
+//
+
+func createRentalCar(passengers: Int) -> String?{
+    var car:RentalCar?
+
+    switch (passengers) {
+    case 0...2:
+        car = Sports()
+    case 3...4:
+        car = Compact()
+    case 5...7:
+        car = SUV()
+//    case 8...20:
+//        car = Bus()
+    default:
+        car = nil;
+    }
+    return car?.name
+}
+
+
 import Foundation
+
+protocol RentalCar{
+    var name: String {get}
+    var passenger: Int {get}
+}
+
+class Compact: RentalCar{
+    var name: String = "swift"
+    var passenger: Int = 4
+}
+
+class Sports: RentalCar{
+    var name: String = "porche"
+    var passenger: Int = 2
+}
+
+class SUV: RentalCar{
+    var name: String = "innova"
+    var passenger: Int = 4
+}
+
+//class Bus: RentalCar{
+//    var name: String = "Bus"
+//    var passenger: Int = 20
+//}
